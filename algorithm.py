ed607e09bf014d6264d86d539c82f121b435d1d0
LittleAksMax <david2005.rosental@gmail.com>
2021-02-22 13:38:39 +0000

    Perfected Console, added functionality added all the files needed

---
+++ algorithm.py
@@ -0,0 +1,16 @@
+
+def astar(drawFunction):
+    print("A*")
+    drawFunction()
+
+def bfs(drawFunction):
+    print("bfs")
+    drawFunction()
+
+def dfs(drawFunction):
+    print("dfs")
+    drawFunction() 
+
+def dijkstra(drawFunction):
+    print("dijkstra's")
+    drawFunction()
