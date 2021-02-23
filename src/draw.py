b588fb59bcce815f78ca16208e3b15c6300e9dcc
LittleAksMax <david2005.rosental@gmail.com>
2021-02-23 18:10:46 +0000

    Done movement, can recenter by pressing Enter

--- src/draw.py
+++ src/draw.py
@@ -2,8 +2,9 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from OpenGL_accelerate import *
 
-from constants import WIDTH, HEIGHT
+from constants import *
 
 WHITE = (255, 255, 255)
 
@@ -31,24 +32,57 @@ def visualize(vertices, edges):
     win = create_window()
 
     # set up player perspective
-    gluPerspective(100, WIDTH//HEIGHT, 0.1, 400) # FOV (deg), aspect ratio (width/height), clipping ranges
-                                         # at what distances between z co-ordinates do you start and stop seeing an object
-    
-    glTranslatef(0, 0, -325) # x, y, z movement
-    #glRotatef(0, 0, 0, 0) # deg, x, y, z 
+    gluPerspective(100, WIDTH//HEIGHT, Z_MIN_CLIP, Z_MAX_CLIP) # FOV (deg), aspect ratio (width/height), clipping ranges
+                                         # at what distances between z co-ordinates do you start and stop seeing an object  
+    x = X_DEFAULT
+    y = Y_DEFAULT
+    z = Z_DEFAULT
+    move = 5
+    scroll = 10
 
+    glTranslatef(x, y, z) # x, y, z movement
+    #glRotatef(0, 0, 0, 0) # deg, x, y, z 
     run = True
     while run:
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT) # clear slate between flames
+
+        #glGetDoublev(GL_MODELVIEW_MATRIX)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4:
+                    z += scroll
+                    glTranslatef(0, 0, scroll)
+                elif event.button == 5:
+                    z -= scroll
+                    glTranslatef(0, 0, -1 * scroll)
         
         #for i in range(len(edges)): # same as len(vertices) and len(Node.nodes)
         #    p1 = (vertices[edges[i][0]][0], vertices[edges[i][0]][1])
         #    p2 = (vertices[edges[i][1]][0], vertices[edges[i][1]][1])
         #    pygame.draw.line(win, WHITE, p1, p2)
-        
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT) # clear slate between flames
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            x += move
+            glTranslatef(move, 0, 0)
+        if keys[pygame.K_UP]:
+            y -= move
+            glTranslatef(0, -1 * move, 0)
+        if keys[pygame.K_DOWN]:
+            y += move
+            glTranslatef(0, move, 0)
+        if keys[pygame.K_RIGHT]:
+            x -= move
+            glTranslatef(-1 * move, 0, 0)
+        if keys[pygame.K_RETURN]: # Enter
+            # reset co-ordinates
+            glTranslatef(X_DEFAULT - x, Y_DEFAULT - y, Z_DEFAULT - z)
+            x = X_DEFAULT
+            y = Y_DEFAULT
+            z = Z_DEFAULT
 
         drawLines(vertices, edges)
 
